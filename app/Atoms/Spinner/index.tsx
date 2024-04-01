d670d04c8fe55d4f399f02ce64445b64a92bb9df
david-lorenzo-vargas <dlorenzo.05@gmail.com>
2024-04-01 17:38:26 +0100

    Spinner atom component

---
+++ app/Atoms/Spinner/index.tsx
@@ -0,0 +1,11 @@
+import { ReactElement } from "react";
+
+import { CircularProgress } from "@mui/material";
+
+const Spinner = (): ReactElement => {
+  return (
+    <CircularProgress />
+  );
+};
+
+export default Spinner;
