e6c771567c2d8b2f773364b119494ff8f1d23581
david-lorenzo-vargas <dlorenzo.05@gmail.com>
2024-04-01 17:41:32 +0100

    EventCard Body molecule component

---
+++ app/Molecules/EventCard/EventCardBody/index.tsx
@@ -0,0 +1,47 @@
+import { ReactElement } from "react";
+
+import Image from "next/image";
+import Link from "next/link";
+
+const EventCardBody = ({
+  title,
+  index,
+  dates,
+  slug,
+}: Props): ReactElement => {
+  return (
+    <Link href={`pages/event/${slug}`}>
+      <div className="mb-20 rounded-xl overflow-hidden bg-blue h-200">
+        <Image
+          src={`https://source.unsplash.com/featured/concert600x60${index}?q=80?orientation=landscape`}
+          alt="event-image"
+          width={600}
+          height={250}
+          className="h-200"
+        />
+      </div>
+      <div>
+        <span>{title}</span>
+      </div>
+      <div>
+        <span className="text-xs text-black opacity-60">
+          {dates}
+        </span>
+      </div>
+      <div>
+        <span className="text-xs font-semibold">
+          London
+        </span>
+      </div>
+    </Link>
+  );
+};
+
+interface Props {
+  title: string;
+  index: number;
+  dates: string;
+  slug: string;
+}
+
+export default EventCardBody;
