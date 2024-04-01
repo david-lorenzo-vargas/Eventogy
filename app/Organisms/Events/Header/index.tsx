fe498371c685e731c70163bfb7ba3fce4888ec48
david-lorenzo-vargas <dlorenzo.05@gmail.com>
2024-04-01 17:45:26 +0100

    Header component for Events Organism

---
+++ app/Organisms/Events/Header/index.tsx
@@ -0,0 +1,13 @@
+import { ReactElement } from "react";
+
+const Header = (): ReactElement => {
+  return (
+    <div className="py-20 md:px-40 px-10">
+      <h1 className="text-2xl font-bold">
+        Event Hub
+      </h1>
+    </div>
+  );
+};
+
+export default Header;
