a9616b95a3e71bc7e5f0f56653874dec11298f4b
david-lorenzo-vargas <dlorenzo.05@gmail.com>
2024-04-01 17:34:17 +0100

    Home page

---
+++ app/page.tsx
@@ -0,0 +1,12 @@
+import Events from "./Organisms/Events";
+
+const Home = () => {
+  return (
+    <div className="h-screen h-screen bg-white">
+      <Events />
+    </div>
+  );
+};
+
+export default Home;
+
